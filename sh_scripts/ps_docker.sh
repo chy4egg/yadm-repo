a505c7a8e526c39cbc7eff3c319dbd7c1251f46c
Aleksandr <chy4egg@gmail.com>
2019-07-12 23:53:40 +0300

    save

--- sh_scripts/ps_docker.sh
+++ sh_scripts/ps_docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 if [ "$1" == "start" ]; then
-  docker-machine start;
-  eval "docker-machine env";
-  docker-compose up -d;
-  docker-compose restart;
+  docker-machine start &&
+  eval "docker-machine env" &&
+  docker-compose up -d &&
+  docker-compose restart
 elif [ "$1" == "stop" ]; then
-  docker-compose stop;
-  docker-machine stop;
+  docker-compose stop &&
+  docker-machine stop
+elif [ "$1" == "restart" ]; then
+  eval "docker-machine env" &&
+  docker-compose restart
 else
   echo "arguments required (start or stop)"
 fi
